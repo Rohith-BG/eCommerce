ee68c197ba73c5b4498122ed6764ea191eaf87d9
ROHITH BG <rohithbg18@gmail.com>
2025-05-06 17:46:41 +0530

    Implemented models

---
+++ src/models/userModel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    userRole:{
+        type:String,
+    },
+    createdAt:{
+        type:new Date();
+    },
+    updatedBy:{
+        type:String
+    }
+})
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+    try{
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(this.password,salt);
+        this.password = hashedPassword ;
+    }
+    catch(err){
+        next(err);
+    }  
+})
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
