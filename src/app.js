32a9ae8c6238e341c73911994bbf7d3a505f4820
Rohith-BG <rohithbg18@gmail.com>
2025-07-17 22:18:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use('/user',userRoute);
 app.use('/partner',partnerRoute);
-app.use('/api',categoryRoute);
+app.use('/',categoryRoute);
 app.use('/product',productRoute);
 
 export default app;
